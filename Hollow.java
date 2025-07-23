4eeb9171261ff14c52cef3d606ad711d10110efa
venkatkudipudi <vnktkudipudi@gmail.com>
2025-07-23 17:29:53 +0530

    Add files via upload

---
+++ Hollow.java
@@ -0,0 +1,23 @@
+class Hollow
+{
+	public static void main(String args[])
+	{
+		int m=5;
+		int n=4;
+		for(int i=1; i<=m; i++)
+		{
+			for(int j=1; j<=n; j++)
+			{
+				if(i==1 || j==1|| i==m|| j==n)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}	
+			}	
+			System.out.println();
+		}
+	}
+}
