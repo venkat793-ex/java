317221c61815bb9e24c7545116dfea0ad0582fa8
venkatkudipudi <vnktkudipudi@gmail.com>
2025-09-08 18:00:31 +0530

    Add files via upload
    Created a simple user friendly chart bot

---
+++ Chartbot.java
@@ -0,0 +1,20 @@
+// simple chartbot 
+import java.util.Scanner;
+class Chartbot
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter your name sir/medam ?");
+		String name =sc.nextLine();
+		System.out.printf("hey %s, how are you", name);
+		String adress =sc.nextLine();
+		System.out.println("Enter your mobile number");
+		String number=sc.nextLine();
+		System.out.println("raise your problem");
+		String content=sc.nextLine();
+		System.out.println("one of our member will contact you have a nice day ");
+	}
+}
+
+		
